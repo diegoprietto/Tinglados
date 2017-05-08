6fcac5d2b7480ccd8590876efc364646c6b57898
diegoprietto <dprsoftso@gmail.com>
2017-05-08 13:34:39 -0300

    Creación Layout

--- app.js
+++ app.js
@@ -7,11 +7,6 @@ var qux = new Qux();
 var AccesoMongo = require('./AccesoMongo.js').Qux;
 var accesoMongo = new AccesoMongo();
 
-
-////Memoria cache (TEST)
-var cache = null;
-
-
 //Definición de puerto
 app.set('port', (process.env.PORT || 5000));
 
@@ -20,9 +15,6 @@ app.use(express.static('public'));
 
 //Usar el paquete Pug para Templates
 app.set('view engine', 'pug');
-
-////Atributos (TEST)
-//var uri = "mongodb://dprbd:w8vdLyC0VNhkfhXm@cluster0-shard-00-00-ngi72.mongodb.net:27017,cluster0-shard-00-01-ngi72.mongodb.net:27017,cluster0-shard-00-02-ngi72.mongodb.net:27017/tinglado?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
 
 //INICIO Funciones AJAX**************************************************************************************
 
@@ -36,22 +28,6 @@ app.get('/getTel', function(req, res){
 
 //FIN Funciones AJAX**************************************************************************************
 
-//INICIO Funciones MongoDB**************************************************************************************
-
-/*var findDocuments = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('Tinglados');
-  // Find some documents
-  collection.find({}).toArray(function(err, docs) {
-    
-    console.log("Found the following records");
-    console.log(docs)
-    callback(docs);
-  });
-}*/
-
-//FIN Funciones MongoDB**************************************************************************************
-
 //INICIO Server**************************************************************************************
 
 //Renderizar usando Pug
@@ -62,64 +38,16 @@ app.get('/', function(req, res){
   });
 });
 
-////TEST CACHE
-app.get('/setcache', function(req, res){
-  var hayDatos = (cache != null)
-  var texto;
-
-  if (hayDatos){
-    var cacheAnterior = cache;
-    cache += '*';
-    texto = 'La cache poseia estos datos ' + cacheAnterior + ', ahora contiene ' + cache;
-  }
-  else{
-    cache = '#';
-    texto = 'La cache estaba VACÍA, ahora contiene ' + cache;
-  }
-
-  res.send(texto);
+//Administración del sitio
+app.get('/admin', function(req, res){
+  res.render('admin');
 });
 
-////TEST CACHE ARCHIVO PLANO
-app.get('/setcachearc', function(req, res){
-  var datos = "Pirulo";
-
-  //Guardar texto en el archivo e informar
-  qux.almacenar(
-    () =>{
-      console.log('Error');
-
-      res.send("Se produjo un error al intentar escribir el archivo");
-    },
-    datos,
-    () => {
-      console.log('Exito');
-
-      res.send("Archivo actualizado!");
-    }
-  );
-});
-
-app.get('/getcachearc', function(req, res){
-
-  //Obtener datos
-  qux.obtener(
-    () =>{
-      console.log('Error');
-
-      res.send("Se produjo un error al intentar leer el archivo");
-    },
-    (data) => {
-      console.log('Exito');
-
-      res.send("Datos del archivo: " + data);
-  });
-});
-
-//Cualquier url que no existente
+//Cualquier url que no existente, redirigir a Home
 app.all('/*', function (req, res) {
-   res.send('Has ingresado una url inexistente!');
    console.log("Acceso a url inexistente");
+
+   res.redirect('/');
 })
 
 var server = app.listen(app.get('port'), function () {
