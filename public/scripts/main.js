e00b3fa2909bf5a560be0acac7f80f57cc20b508
diegoprietto <dprsoftso@gmail.com>
2017-05-28 10:03:03 -0300

    Agregado de botón Me Gusta (Modo Test)

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,15 @@
 "use strict";
 
 $(document).ready(function () {
+	//Scrips de Facebook
+	$("#scriptFace").append("(function(d, s, id) {\
+	  var js, fjs = d.getElementsByTagName(s)[0];\
+	  if (d.getElementById(id)) return;\
+	  js = d.createElement(s); js.id = id;\
+	  js.src = '//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.9&appId=805247039540795';\
+	  fjs.parentNode.insertBefore(js, fjs);\
+	}(document, 'script', 'facebook-jssdk'));");
+
 	actualizarAltoCarrusel();
 	$(".botonLlamar").on( "click", llamarTel );
 	//Solicitar fotos del server para el carrousel
