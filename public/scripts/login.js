97f3aac8fb24da7b6b87a0bad10ca237cc4d108a
diegoprietto <dprsoftso@gmail.com>
2017-06-02 17:13:50 -0300

    Se corrigió el estilo y un bug de la vista login

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -20,36 +20,41 @@ function logOk(resultado){
 	if (resultado && resultado.Resultado){
 		switch(resultado.Resultado){
 			case "INCOMPLETE":
-				alert("Complete ambos campos");
+				mostrarAlerta("Complete ambos campos");
 				habilitarInit();
 			break;
 			case "ERROR":
-				alert("Se produjo un error en las bases de datos del servidor, recargue la página y vuelva a reintentar, si el problema persiste contacte al desarrollador del sitio para su revisión.");
+				mostrarAlerta("Se produjo un error en las bases de datos del servidor, recargue la página y vuelva a reintentar, si el problema persiste contacte al desarrollador del sitio para su revisión.");
 				habilitarInit();
 			break;
 			case "NOTFIND":
-				alert("El usuario y/o contraseña no son válidos");
+				mostrarAlerta("El usuario y/o contraseña no son válidos");
 				habilitarInit();
 			break;
 			case "OK":
 				window.location="/admin";
 			break;
 			default:
-				alert("Respuesta no válida del servidor, recargue la página y vuelva a reintentar, si el problema persiste contacte al desarrollador del sitio para su revisión.");
+				mostrarAlerta("Respuesta no válida del servidor, recargue la página y vuelva a reintentar, si el problema persiste contacte al desarrollador del sitio para su revisión.");
 				habilitarInit();
 			break;
 		}
 
 	}else{
-		alert("Respuesta no válida del servidor, recargue la página y vuelva a reintentar, si el problema persiste contacte al desarrollador del sitio para su revisión.");
+		mostrarAlerta("Respuesta no válida del servidor, recargue la página y vuelva a reintentar, si el problema persiste contacte al desarrollador del sitio para su revisión.");
 	}
 }
 
 function LogError(){
-	alert("Se produjo un error en el servidor, recargue la página y vuelva a reintentar, si el problema persiste contacte al desarrollador del sitio para su revisión.");
+	mostrarAlerta("Se produjo un error en el servidor, recargue la página y vuelva a reintentar, si el problema persiste contacte al desarrollador del sitio para su revisión.");
 	habilitarInit();
 }
 
 function habilitarInit(){
-	$("#BotonGuardarInfo").removeAttr("disabled");
+	$("#init").removeAttr("disabled");
+}
+
+function mostrarAlerta(msj){
+	$("#AlertError span").text(msj);
+	$("#AlertError").show();
 }
